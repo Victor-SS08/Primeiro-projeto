bbec9e862f8d96eba04e6e6800c879e41083a22e
Victor <victorsilvasouto08@gmail.com>
2026-03-30 09:17:47 -0300

    feat:declaracoes e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
-alert("hello world!")
+// declaracoes e variaveis
+//var, let e const
+// const é um valor constante, nao pode ser alterado
+var nome = "fiap";
+console.log(nome);
+
+var nome ="teste";
+console.log(nome);
+
+if(true){
+ let apelido = "fiapinho";
+ console.log(apelido)
+}
+ 
+ const taxajuros= 0.05;
+ console.log(taxajuros);
+ //taxajuros=1.5;
+var exemplo = "ola-dev";
+console.log(typeof exemplo)
+let exemplo1= "ola-dev-let";
+console.log(exemplo1)
+const exemplo2="ola-dev-const";
+console.log(exemplo2)
+let exemplo3=10;
+console.log(exemplo3);
+let exemplo4= true
+console.log(typeof exemplo4);
+let exemplo5= {nome1:"teste"};
+console.log(typeof exemplo5)
+let exemplo6=["Java","C#","PHP"];
+console.log(exemplo6)
+//valor undefined
+let exemplo7;
+console.log(exemplo7)
+let exepmlo8= null;
+console.log(exepmlo8)
